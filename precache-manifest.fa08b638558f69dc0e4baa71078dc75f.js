59cb752e28b4bf414ea58d8b422c417388e07ee8
Matan Crispel <matan.cris@gmail.com>
2021-07-12 18:03:57 +0300

    add dist

---
+++ precache-manifest.fa08b638558f69dc0e4baa71078dc75f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41105a43f2e878ab67e4",
+    "url": "/TeamManagerVue/css/app.05a1da54.css"
+  },
+  {
+    "revision": "555d244d9fb74d2545d268ff3a9f9e44",
+    "url": "/TeamManagerVue/img/logo.555d244d.png"
+  },
+  {
+    "revision": "107d3a15edbe5b6c2e69c7aebf361d2b",
+    "url": "/TeamManagerVue/index.html"
+  },
+  {
+    "revision": "f24b33bff9823a4289a3",
+    "url": "/TeamManagerVue/js/about.0d70864e.js"
+  },
+  {
+    "revision": "41105a43f2e878ab67e4",
+    "url": "/TeamManagerVue/js/app.a5973059.js"
+  },
+  {
+    "revision": "ed4a78547e335da3d537",
+    "url": "/TeamManagerVue/js/chunk-vendors.01430a0d.js"
+  },
+  {
+    "revision": "b33bc168431a45f19c271514eefa37e1",
+    "url": "/TeamManagerVue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TeamManagerVue/robots.txt"
+  }
+]);
