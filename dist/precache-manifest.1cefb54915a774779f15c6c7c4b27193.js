7410a9ece14633febe8371a52e8af4c3c1950f19
Matan Crispel <matan.cris@gmail.com>
2021-11-23 14:29:45 +0200

    adding dist subtree

---
+++ dist/precache-manifest.1cefb54915a774779f15c6c7c4b27193.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bd90a8b03bf73e89f3",
+    "url": "/TeamManagerVue/css/app.ed1b6870.css"
+  },
+  {
+    "revision": "75b4247fdd3b97d0e3b8e07b115673c2",
+    "url": "/TeamManagerVue/fonts/Raleway-Regular.75b4247f.ttf"
+  },
+  {
+    "revision": "fece1dfff07a41432d3846a04432af13",
+    "url": "/TeamManagerVue/img/404-img.fece1dff.jpg"
+  },
+  {
+    "revision": "ffed94e1679057cb6bd3fe78552eb4a6",
+    "url": "/TeamManagerVue/img/404-img.ffed94e1.png"
+  },
+  {
+    "revision": "0665f8de4256ae10a8969c43bbac35dc",
+    "url": "/TeamManagerVue/img/Home-img.0665f8de.jpg"
+  },
+  {
+    "revision": "03ec8662cc302af750c296c2670bf917",
+    "url": "/TeamManagerVue/img/avatar-placeholder.03ec8662.jpg"
+  },
+  {
+    "revision": "d5dc8a64bb7df0b088d6de475feb2c69",
+    "url": "/TeamManagerVue/img/avatar-placeholder.d5dc8a64.png"
+  },
+  {
+    "revision": "054dce1ff45ca6023ad88975c5345b75",
+    "url": "/TeamManagerVue/img/chart-bgi.054dce1f.jpg"
+  },
+  {
+    "revision": "611dc3ddfbe004b1536ca6fdf4e1b0d6",
+    "url": "/TeamManagerVue/img/chart-bgi.611dc3dd.png"
+  },
+  {
+    "revision": "2f08c92e640e3ba022c9ac640974a6c6",
+    "url": "/TeamManagerVue/img/clock-bgi.2f08c92e.jpg"
+  },
+  {
+    "revision": "90958c1d286d9d49c74674de97336814",
+    "url": "/TeamManagerVue/img/hadas-img.90958c1d.png"
+  },
+  {
+    "revision": "f97549ffad6df95f8c30c21c3549728e",
+    "url": "/TeamManagerVue/img/logo.f97549ff.png"
+  },
+  {
+    "revision": "eb2174b57fbdaf36acfc3dcd63aa0223",
+    "url": "/TeamManagerVue/img/matan-img.eb2174b5.png"
+  },
+  {
+    "revision": "1e350a0605cffb91dbfb06d2030c5824",
+    "url": "/TeamManagerVue/img/wave-bgi.1e350a06.jpg"
+  },
+  {
+    "revision": "c04732ea01d4b58937ed1d8df5f28f0f",
+    "url": "/TeamManagerVue/img/wave-bgi.c04732ea.png"
+  },
+  {
+    "revision": "a0f8a08720085646a7260d68af4f148a",
+    "url": "/TeamManagerVue/index.html"
+  },
+  {
+    "revision": "a75561202105f40ac518",
+    "url": "/TeamManagerVue/js/about.ef5be4f9.js"
+  },
+  {
+    "revision": "d4bd90a8b03bf73e89f3",
+    "url": "/TeamManagerVue/js/app.f9eec4cb.js"
+  },
+  {
+    "revision": "6cadbf0a0b830b4f669f",
+    "url": "/TeamManagerVue/js/chunk-vendors.1dca98a6.js"
+  },
+  {
+    "revision": "b33bc168431a45f19c271514eefa37e1",
+    "url": "/TeamManagerVue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TeamManagerVue/robots.txt"
+  }
+]);
